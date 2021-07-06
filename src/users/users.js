384b03a69eb3373cacd88bd122eeb3e2a9358515
mind <ronak.jain@mindbowser.com>
2021-07-06 16:23:06 +0530

    first commit working fine

---
+++ src/users/users.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+
+class Users extends Component {
+    state = {  }
+    render() { 
+        return ( 
+            <div>
+                this is working...
+            </div>
+         );
+    }
+}
+ 
+export default Users;
